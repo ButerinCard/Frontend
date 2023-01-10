03a1fb4b8d99c62d2dacbb75813ad16bcf0b1d03
TarpCityBitch <travistarpley0@gmail.com>
2023-01-10 13:46:31 -0800

    Background Graphic

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,10 @@ import Navbar from '../components/Navbar';
 import MintButton from '../components/Landing/MintButton';
 import ButerinCard from '../components/Landing/ButerinCard';
 import card2 from '../public/images/backOfCard.png';
+import card3 from '../public/images/cardBacks/back0.png'
 import proto from '../public/images/Proto.png';
 import card from '../public/images/Card.png'
+import Graphic from '../public/images/graphic.png'
 
 import { useEffect, useState } from 'react';
 import Deck from '../components/Landing/Deck';
@@ -21,7 +23,7 @@ export default function Home() {
       }, 700); 
     }
   }, [load])
-  let cards = [card2, proto, card];
+  let cards = [card2, proto, card, card3];
 
   function goBack() {
     if (i >= cards.length - 1)
@@ -43,11 +45,14 @@ export default function Home() {
   const cardProps = { goBack, goFoward, load }
 
   return (
-    <>
+    <div className='overflow-x-hidden' style={{ background: '#e3dbcb' }}>
       <Navbar></Navbar>
-      <main className='w-screen h-screen relative bg-black' style={{ background: '#e3dbcb' }} >
+      <main className='w-screen h-screen relative '  >
+        <div className='absolute w-screen h-screen top-0 left-0'>
+          <Image src={Graphic} className='opacity-30' alt=''></Image>
+        </div>
         <MintButton />
-        <div className='z-0 absolute px-20 h-screen w-screen grid grid-cols-3 items-center justify-center'>
+        <div className='z-0 absolute lg:px-12 xl:px-20 h-screen w-screen grid lg:grid-cols-3 items-center justify-center'>
           <Deck />
           {cards.map((e, index, arr) => {
             if (i === index) {
@@ -64,7 +69,7 @@ export default function Home() {
 
           <div className='flex justify-center'>
             <div>
-              <h1 className='text-center font-orbitron font-semibold mb-3'>TOP MINERS</h1>
+              <h1 className='text-center font-orbitron font-semibold mb-3 select-none'>TOP MINERS</h1>
               <div className=' py-1 px-4 rounded-md' style={{ background: '#ebe4d8', width: '352px', height: '389px' }}>
                 <LeaderBoardEntry />
                 <LeaderBoardEntry />
@@ -78,7 +83,7 @@ export default function Home() {
         </div>
       </main>
 
-    </>
+    </div>
   )
 }
 
