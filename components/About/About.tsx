6bb010aa0fe5326f1db9a13ac6d57a39a0659da2
TarpCityBitch <travistarpley0@gmail.com>
2023-03-01 00:12:26 -0800

    Adding Hover

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,21 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 import background from '../../public/images/background.png'
 import aboutCard from '../../public/images/aboutCard.png';
 import Image from 'next/image';
 export default function About() {
+    const [phase, setPhase] = useState(false);
+    const [pic, setPic] = useState(false);
+    const [body, setBody] = useState(false);
+    const [size, setSize] = useState(false);
     return (
         <>
             {/* About Sections */}
             < div className='font-PS2 py-10' style={{ backgroundImage: `url(${background.src})` }
             }>
                 <section className='bg-transparent'>
-
-
                     <h1 className='text-center' >About</h1>
+                    <div className='rounded-lg relative my-12 2xl:mx-80 bg-black bg-opacity-5 flex justify-center py-5'>
 
-                    <div className='rounded-lg my-10 2xl:mx-80 bg-black bg-opacity-5 flex flex-col items-center py-5' >
-                        <h2 className='font-mono mt-2 mb-2'>(Hover over areas to learn more)</h2>
-                        <Image src={aboutCard} alt='About Card' width={300} height={420} />
+                        <div className={`w-1/3 h-44  transition-opacity `}>
+                            <div className={`flex hover:opacity-100 ${pic ? 'opacity-100' : 'opacity-0'}`}>
+                                <div className={`pl-10 mt-28`} style={{ fontSize: '10px' }}>
+                                    <p >An AI generated image from Vitalik with many crypto references in the background, including DeFi, NFTs and others</p>
+                                    <br />
+                                    <p>This image will go through different phases as it is uploaded onchain.</p>
+                                </div>
+                                <h1 className='mt-28 whitespace-nowrap'> -&gt; </h1>
+                            </div>
+                            <div className={`flex hover:opacity-100 ${size ? 'opacity-100' : 'opacity-0'}`}>
+                                <div className={`pl-10 mt-28 `} style={{ fontSize: '10px' }}>
+                                    <p >An AI generated image from Vitalik with many crypto references in the background, including DeFi, NFTs and others</p>
+                                    <br />
+                                    <p>This image will go through different phases as it is uploaded onchain.</p>
+                                </div>
+                                <h1 className='mt-48 whitespace-nowrap'> -&gt; </h1>
+                            </div>
+
+                        </div>
+
+
+                        <div className="flex relative">
+                            <div>
+                                <h2 className='font-mono mb-2'>(Hover over areas to learn more)</h2>
+                                <Image src={aboutCard} alt='About Card' width={300} height={420} />
+                                <div onMouseOver={() => setPhase(true)} onMouseOut={() => setPhase(false)} className="absolute h-10 w-full bg-red-400 bg-opacity-30 top-10"></div>
+                                <div onMouseOver={() => setPic(true)} onMouseOut={() => setPic(false)} className="absolute h-48 w-full bg-green-400 bg-opacity-30 top-20"></div>
+                                <div onMouseOver={() => setBody(true)} onMouseOut={() => setBody(false)} className="absolute h-32 w-full bg-opacity-30 bg-blue-400 bottom-10 "></div>
+                                <div onMouseOver={() => setSize(true)} onMouseOut={() => setSize(false)} className="absolute h-10 w-full bg-opacity-30 bg-red-400 bottom-1"></div>
+                            </div>
+                        </div>
+
+                        <div className='flex w-1/3'>
+
+                        </div>
+
                     </div>
                 </section>
 
@@ -24,7 +60,7 @@ export default function About() {
                     <div className='rounded-lg my-10 2xl:mx-80 bg-black bg-opacity-5 font-mono py-10 px-8 lg:px-52 text-sm sm:text-lg ' >
                         <p>This is an NFT, acting as a homage to Vitalik. In total there will be 2015 cards.</p>
                         <br></br>
-                       
+
                         <p>The card itself will be an SVG but the main image will be an embedded JPEG. A JPEG with the size of 16 MB, stored across 2015 contracts. This is the biggest JPEG stored on-chain.</p>
                         <br />
                         <p>The minters will upload 1/2015 chunk of approximately 11kB when minting. In return they will get the card, but as you might see in the image they will all be different even if based on the same image. </p>
@@ -35,7 +71,7 @@ export default function About() {
                 </section>
 
                 <section className='bg-transparent'>
-                    <h1 className='text-center' >About</h1>
+                    <h1 className='text-center' >Team</h1>
                     <div className='rounded-lg my-10 mx-80 bg-black bg-opacity-5' style={{ height: '433px' }}>
 
                     </div>
